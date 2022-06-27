8dd3549eb94d8e2c75b21c9582a6351951aba5fd
Juliya <juliyaone123@gmail.com>
2022-06-27 21:29:17 +0300

    react.memo()

--- my-social-network/src/Components/News/News.jsx
+++ my-social-network/src/Components/News/News.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useState,useEffect } from 'react';
 import './News.css';
 
-function Counter() {
+const Counter = React.memo((props) => {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
@@ -13,12 +13,13 @@ function Counter() {
 
   return (
     <div>
+      {console.log('render')}
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>
         Click me
       </button>
     </div>
   );
-}
+})
 
 export default Counter;
